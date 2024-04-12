abab9706f13ecfe6ff452fc3d9eca8c7ca680edc
AnitaNoohian <noohian83@gmail.com>
2024-04-12 16:58:31 +0330

    push

--- src/main/java/Admin.java
+++
@@ -1,8 +0,0 @@
-public class Admin extends Subreddit{
-    public Admin(String name, Account host) {
-        super(name, host);
-    }
-    public void addAdmin(Account user){
-        admins.add(user);
-    }
-}
